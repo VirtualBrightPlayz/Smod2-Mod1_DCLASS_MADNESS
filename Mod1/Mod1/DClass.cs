3cca9ddccb8b7cf3644c0b4a42c93b6f1eff08b0
Brian Zulch <lego.trains.b@gmail.com>
2018-07-22 12:09:53 -0700

    inital

---
+++ Mod1/Mod1/DClass.cs
@@ -0,0 +1,30 @@
+﻿using Smod2;
+using Smod2.Attributes;
+
+namespace VirtualBrightPlayz.SCPSL.Mod1
+{
+    [PluginDetails(author = "VirtualBrightPlayz",
+        description = "D-Class vs. SCP-173",
+        id = "virtualbrightplayz.scpsl.mod1",
+        name = "D-Class Madness",
+        version = "1.0",
+        SmodMajor = 3,
+        SmodMinor = 0,
+        SmodRevision = 0)]
+    public class DClass : Plugin
+    {
+        public override void OnDisable()
+        {
+        }
+
+        public override void OnEnable()
+        {
+            this.Info("D-Class Madness plugin enabled.");
+        }
+
+        public override void Register()
+        {
+            this.AddEventHandlers(new DClassEventHandler1(this));
+        }
+    }
+}
---
+++ Mod1/Mod1/DClassEventHandler1.cs
@@ -0,0 +1,68 @@
+﻿using Smod2;
+using Smod2.API;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using System.Collections.Generic;
+
+namespace VirtualBrightPlayz.SCPSL.Mod1
+{
+    internal class DClassEventHandler1 : IEventHandlerSetRole, IEventHandlerRoundRestart, IEventHandlerPlayerDie, IEventHandlerRoundStart
+    {
+        private Plugin plugin;
+        private bool oneSevenThreeSpawned;
+        private bool roundStarted;
+        private List<int> players = new List<int>();
+
+        public DClassEventHandler1(Plugin dClass)
+        {
+            this.plugin = dClass;
+            oneSevenThreeSpawned = false;
+            roundStarted = false;
+            players.Clear();
+        }
+
+        void IEventHandlerPlayerDie.OnPlayerDie(PlayerDeathEvent ev)
+        {
+            players.Remove(ev.Player.PlayerId);
+        }
+
+        void IEventHandlerRoundRestart.OnRoundRestart(RoundRestartEvent ev)
+        {
+            oneSevenThreeSpawned = false;
+            roundStarted = false;
+            players.Clear();
+        }
+
+        void IEventHandlerRoundStart.OnRoundStart(RoundStartEvent ev)
+        {
+            roundStarted = true;
+        }
+
+        void IEventHandlerSetRole.OnSetRole(PlayerSetRoleEvent ev)
+        {
+            if (roundStarted && ev.Role == Role.CHAOS_INSUGENCY)
+            {
+                ev.Player.ChangeRole(Role.NTF_COMMANDER);
+            }
+            else if (ev.Role == Role.SPECTATOR || players.Contains(ev.Player.PlayerId))
+            {
+                return;
+            }
+            else if (!players.Contains(ev.Player.PlayerId))
+            {
+                plugin.Info("Spawning player " + ev.Player.Name);
+                if (!oneSevenThreeSpawned)
+                {
+                    players.Add(ev.Player.PlayerId);
+                    oneSevenThreeSpawned = true;
+                    ev.Player.ChangeRole(Role.SCP_173);
+                }
+                else
+                {
+                    players.Add(ev.Player.PlayerId);
+                    ev.Player.ChangeRole(Role.CLASSD);
+                }
+            }
+        }
+    }
+}
